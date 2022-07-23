801e015fc5fe19bc8e3a1be85ccdb6b9d7a63716
Muhammad Sameed Abbasi <msameedabbasi@gmail.com>
2022-07-23 19:24:44 +0500

    ss save

--- Subsidized Fueling System/Display_info.cs
+++ Subsidized Fueling System/Display_info.cs
@@ -13,21 +13,25 @@ using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
 using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace Subsidized_Fueling_System
 {
     public partial class Display_info : Form
     {
+        bool ml_bool;
         public static Bitmap vdo_img;
         public Display_info()
         {
             InitializeComponent();
         }
+
         FilterInfoCollection filter;
         VideoCaptureDevice device;
 
         private void Display_info_Load(object sender, EventArgs e)
         {
+            ml_bool = true;
             vdo_img = null;
             filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
             foreach (FilterInfo device in filter)
@@ -36,37 +40,60 @@ namespace Subsidized_Fueling_System
             device = new VideoCaptureDevice();
 
         }
-
-        //Thread thread;
-
+        
         private void start_video_Click(object sender, EventArgs e)
         {
-            //thread = new Thread(video_ML);
-
-
             device = new VideoCaptureDevice(filter[video_list_combobox.SelectedIndex].MonikerString);
             device.NewFrame += new_frame;
             device.Start();
         }
 
-        //static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        //static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_car.xml");
 
         private void new_frame(object sender, NewFrameEventArgs eventArgs)
         {
             vdo_img = (Bitmap)eventArgs.Frame.Clone();
-
             pictureBox1.Image = vdo_img;
+            if (ml_bool)
+            {
+                ml_bool = false;
+                ml_thread();
+                //ml_bool = true;
+            }
+            Console.WriteLine("ml2---");
 
-            video_ML();
+            //Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            //Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            /*Rectangle[] rectangles = cascadeClassifier.DetectMultiScale((IInputArray)eventArgs.Frame.Clone(), 1.2, 1);
+            foreach (Rectangle rectangle in rectangles)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            pictureBox1.Image = bitmap;*/
 
         }
 
+        private void ml_thread()
+        {
+            Task t1 = new Task(video_ML);
+            t1.Start();
+        }
         private void video_ML()         //ml script
         {
-            pictureBox2.Image = vdo_img;
+            //if (pictureBox2.IsAccessible)
+            //pictureBox2.Image = vdo_img;
+            Thread.Sleep(2000);
+            Console.WriteLine("ml..........");
 
 
 
+            ml_bool = true;
         }
 
         private void Display_info_FormClosing(object sender, FormClosingEventArgs e)
@@ -78,15 +105,19 @@ namespace Subsidized_Fueling_System
         {
             try
             {
+                string datetime = DateTime.Now.ToString("dd/MM/yyyy-HH/mm/ss");
                 if (!Directory.Exists(@"C:\Users\Shumail\Desktop\SFS images"))
                 {
                     Directory.CreateDirectory(@"C:\Users\Shumail\Desktop\SFS images");
-                    MessageBox.Show("Images Folder Created ...");
+                    string path = @"C:\Users\Shumail\Desktop\SFS images";
+                    pictureBox1.Image.Save(path + @"\car image test" +datetime+ ".jpg", ImageFormat.Jpeg);
+                    //MessageBox.Show("Images saved");
                 }
                 else
                 {
                     string path = @"C:\Users\Shumail\Desktop\SFS images";
-                    pictureBox1.Image.Save(path + @"\car image test"  + ".jpg", ImageFormat.Jpeg);
+                    pictureBox1.Image.Save(path + @"\car image test"  +datetime+ ".jpg", ImageFormat.Jpeg);
+                    //MessageBox.Show("Images saved");
                 }
             }
             catch (Exception ex)
@@ -97,12 +128,8 @@ namespace Subsidized_Fueling_System
 
         private void thread_btn_Click(object sender, EventArgs e)
         {
-            save();
-            //Form ml = new Form();
-            //ml.Show();
-            
-            //Thread thread = new Thread(video_ML);
-            //thread.Start();
+            new Task(save).Start();
+            //save();
         }
 
         private void log_btn_Click(object sender, EventArgs e)
